9562abcf7d0214da446a26225db763dc2ffea03e
CrazedDoodler <10962612@uvu.edu>
2022-09-06 19:55:41 -0600

    Create Unit1Lab.cs

---
+++ Unit1Lab.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ian";
+            string lastName = "Clement";
+            Console.WriteLine(name + " " + lastName);
+            int numb = 19;
+            numb = 20; // age is soon 20
+            Console.WriteLine("Age: " + numb);
+
+        }
+    }
+}
