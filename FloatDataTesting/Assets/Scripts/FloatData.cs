ecefa02b6634e4cf1dce61efe43c81638eecb806
CrazedDoodler <10962612@uvu.edu>
2022-10-19 17:26:54 -0600

    Created Event behaviors within FloatDataTest proj.

--- FloatDataTesting/Assets/Scripts/FloatData.cs
+++ FloatDataTesting/Assets/Scripts/FloatData.cs
@@ -6,6 +6,10 @@ public class FloatData : ScriptableObject
 {
     public float value;
 
+    public void ResetValue(float num)
+    {
+        value = num;
+    }
     public void UpdateValue(float num)
     {
         value += num;
---
+++ FloatDataTesting/Assets/Scripts/MonoEventBehavior.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent, awakeEvent, disableEvent;
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        disableEvent.Invoke();
+    }
+}
