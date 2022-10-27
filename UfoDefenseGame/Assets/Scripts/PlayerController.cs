f3ee1ea5c75e3111d378c0e48088147bccce956f
Ian <10962612@uvu.edu>
2022-10-26 19:17:45 -0600

    Created Scripts for movement, projectiles, and object deletion.

---
+++ UfoDefenseGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed;
+    public float xRange;
+    public Transform blaster;
+    public GameObject projectile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // set horizontal to recieve from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+        // moves player left+right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // Keep player in bounds (Left)
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // Keep player in bounds (Right)
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //creates projectile for blaster
+            Instantiate(projectile, blaster.transform.position, projectile.transform.rotation);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
