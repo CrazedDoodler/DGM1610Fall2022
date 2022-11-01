56faf5b16de2cbccb5840581e44d683022290d2c
Ian <10962612@uvu.edu>
2022-11-01 14:24:45 -0600

    Made scripts to spawn Enemies with "S", no camera or Collision yet

---
+++ UfoDefenseGame/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs; // Array to store Enemy Ships
+    private float spawnRangeX = 60f;
+    private float spawnPosZ = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0,spawnPosZ);
+            int enemyIndex = Random.Range(0,enemyPrefabs.Length); // Picks a random enemy
+            Instantiate(enemyPrefabs[enemyIndex],spawnPos, enemyPrefabs[enemyIndex].transform.rotation); // Spawns enemy from array on X
+        }
+    }   
+}
