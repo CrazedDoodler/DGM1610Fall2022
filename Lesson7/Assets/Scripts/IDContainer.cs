90026d75d6e2bd988d1c00b6331406f24e82a7ec
Ian <10962612@uvu.edu>
2022-11-16 12:30:54 -0700

    Added click-drag system, color system, and animations

--- Lesson7/Assets/Scripts/IDContainer.cs
+++ Lesson7/Assets/Scripts/IDContainer.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class IDContainer : MonoBehaviour
 {
     public IDSystem idObj;
+    public UnityEvent startEvent;
 
+    public void Start()
+    {
+        startEvent.Invoke();
+    }
 }
---
+++ Lesson7/Assets/Scripts/SpriteBehavior.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class SpriteBehavior : MonoBehaviour
+{
+    private SpriteRenderer renderObj;
+
+    private void Start()
+    {
+        renderObj = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeRendererColor(ColorID obj)
+    {
+        renderObj.color = obj.value;
+    }
+}
